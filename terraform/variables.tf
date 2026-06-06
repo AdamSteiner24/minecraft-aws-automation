a1391a5570cd487230fdc6bb06ba32a24fcf0594
Adam Steiner <steinead@oregonstate.edu>
2026-06-06 01:55:03 -0700

    Fixed instance type
    Changed from t3.small to t3.micro

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -13,7 +13,7 @@ variable "aws_region" {
 variable "instance_type" {
   description = "EC2 instance type for the Minecraft server."
   type        = string
-  default     = "t3.small"
+  default     = "t3.micro"
 }
 
 variable "allowed_ssh_cidr" {
